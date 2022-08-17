a417261f7e2eb9d7fac9f1d94453f96398429ddb
Luan Novais <luaannovais@live.com>
2022-08-17 19:02:25 -0300

    Feat - Custom logo for dark mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,13 +4,18 @@ import {
   Container,
   Flex,
   HStack,
+  VStack,
+  Text,
   Heading,
 } from '@chakra-ui/react';
 
-import { ReactComponent as LuancodeIcon } from './../../images/luancode-icon.svg';
+import { ReactComponent as LuancodeIconLight } from './../../images/luancode-icon.svg';
+import { ReactComponent as LuancodeIconDark } from './../../images/luancode-icon-dark.svg';
 import ColorModeSwitcher from './../ColorModeSwitcher';
 
 const Header = () => {
+  const LuancodeIcon = useColorModeValue(LuancodeIconLight, LuancodeIconDark);
+
   const bgColor = useColorModeValue(``, `gray.700`);
   const boxShadow = useColorModeValue(`lg`, `md`);
   const headingColor = useColorModeValue(`gunmetal`, `mintCream`);
@@ -23,9 +28,19 @@ const Header = () => {
             <Box w="16">
               <LuancodeIcon />
             </Box>
-            <Heading as="h1" size="sm" color={headingColor}>
-              tools
-            </Heading>
+            <VStack>
+              <Text size="sm" fontWeight="semibold">
+                luancode
+              </Text>
+              <Heading
+                as="h1"
+                size="sm"
+                color={headingColor}
+                sx={{ mt: `0 !important` }}
+              >
+                tools
+              </Heading>
+            </VStack>
           </HStack>
           <ColorModeSwitcher />
         </Flex>
